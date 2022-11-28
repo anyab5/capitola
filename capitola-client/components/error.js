90f754d469d2b18f9c47573a43f5edfe508198df
Anya <anyab5@gmail.com>
2022-11-28 22:55:04 +0200

    minor change :)

--- capitola-client/components/error.js
+++ capitola-client/components/error.js
@@ -1,11 +1,17 @@
 import Layout from "./layout";
 import { Typography } from "@mui/material";
+import Image from "next/image";
 
 export default function Error() {
   return (
     <Layout>
-      <Typography variant="h4" my="40px">Oopps, something unexpected happened..</Typography>
-      <Typography variant="body" mt="40px">Maybe someone fell from a tower :) </Typography>
+      <Typography variant="h5" my="40px">Oopps, something unexpected happened..</Typography>
+        <Image
+        src="/fall-from-tower.jpg"
+        alt="Error"
+        width="650"
+        height="300"
+     />
     </Layout>
   );
 }
